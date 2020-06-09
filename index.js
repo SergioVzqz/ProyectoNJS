c014b7cbafc09f17169b161ab165d8d781e49ea1
Sergio Vazquez <sergiovazag@gmail.com>
2020-06-08 20:05:47 -0500

    *Added* html

---
+++ index.js
@@ -0,0 +1,10 @@
+//Dependencies
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Server is running...');
+});
+
+
